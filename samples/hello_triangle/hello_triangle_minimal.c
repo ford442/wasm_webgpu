c7177841b9280fdea9543d7eb8506820696bce37
Noah Cohn <noahc42@gmail.com>
2023-05-29 21:11:39 -0700

    Update hello_triangle_minimal.c

--- samples/hello_triangle/hello_triangle_minimal.c
+++ samples/hello_triangle/hello_triangle_minimal.c
@@ -6,40 +6,31 @@ WGpuDevice device;
 WGpuQueue queue;
 WGpuRenderPipeline renderPipeline;
 
-EM_BOOL raf(double time, void *userData)
-{
-  WGpuCommandEncoder encoder = wgpu_device_create_command_encoder(device, 0);
-
-  WGpuRenderPassColorAttachment colorAttachment = WGPU_RENDER_PASS_COLOR_ATTACHMENT_DEFAULT_INITIALIZER;
-  colorAttachment.view = wgpu_texture_create_view(wgpu_canvas_context_get_current_texture(canvasContext), 0);
-
-  WGpuRenderPassDescriptor passDesc = {};
-  passDesc.numColorAttachments = 1;
-  passDesc.colorAttachments = &colorAttachment;
-
-  WGpuRenderPassEncoder pass = wgpu_command_encoder_begin_render_pass(encoder, &passDesc);
-  wgpu_render_pass_encoder_set_pipeline(pass, renderPipeline);
-  wgpu_render_pass_encoder_draw(pass, 3, 1, 0, 0);
-  wgpu_render_pass_encoder_end(pass);
-
-  WGpuCommandBuffer commandBuffer = wgpu_command_encoder_finish(encoder);
-
-  wgpu_queue_submit_one_and_destroy(queue, commandBuffer);
-
-  return EM_FALSE; // Render just one frame, static content
+EM_BOOL raf(double time, void *userData){
+WGpuCommandEncoder encoder = wgpu_device_create_command_encoder(device, 0);
+   WGpuRenderPassColorAttachment colorAttachment = WGPU_RENDER_PASS_COLOR_ATTACHMENT_DEFAULT_INITIALIZER;
+   colorAttachment.view = wgpu_texture_create_view(wgpu_canvas_context_get_current_texture(canvasContext), 0);
+   WGpuRenderPassDescriptor passDesc = {};
+   passDesc.numColorAttachments = 1;
+   passDesc.colorAttachments = &colorAttachment;
+   WGpuRenderPassEncoder pass = wgpu_command_encoder_begin_render_pass(encoder, &passDesc);
+   wgpu_render_pass_encoder_set_pipeline(pass, renderPipeline);
+   wgpu_render_pass_encoder_draw(pass, 3, 1, 0, 0);
+   wgpu_render_pass_encoder_end(pass);
+WGpuCommandBuffer commandBuffer = wgpu_command_encoder_finish(encoder);
+wgpu_queue_submit_one_and_destroy(queue,commandBuffer);
+return EM_FALSE; // Render just one frame, static content
 }
 
-void ObtainedWebGpuDevice(WGpuDevice result, void *userData)
-{
-  device = result;
-  queue = wgpu_device_get_queue(device);
+void ObtainedWebGpuDevice(WGpuDevice result,void *userData){
+device=result;
+queue=wgpu_device_get_queue(device);
 
-  canvasContext = wgpu_canvas_get_webgpu_context("canvas");
-
-  WGpuCanvasConfiguration config = WGPU_CANVAS_CONFIGURATION_DEFAULT_INITIALIZER;
-  config.device = device;
-  config.format = navigator_gpu_get_preferred_canvas_format();
-  wgpu_canvas_context_configure(canvasContext, &config);
+   canvasContext = wgpu_canvas_get_webgpu_context("canvas");
+   WGpuCanvasConfiguration config = WGPU_CANVAS_CONFIGURATION_DEFAULT_INITIALIZER;
+   config.device = device;
+   config.format = navigator_gpu_get_preferred_canvas_format();
+   wgpu_canvas_context_configure(canvasContext, &config);
 
   const char *vertexShader =
     "@vertex\n"
@@ -59,40 +50,32 @@ void ObtainedWebGpuDevice(WGpuDevice result, void *userData)
       "return vec4<f32>(1.0, 0.5, 0.3, 1.0);\n"
     "}\n";
 
-  WGpuShaderModuleDescriptor shaderModuleDesc = {};
-  shaderModuleDesc.code = vertexShader;
-  WGpuShaderModule vs = wgpu_device_create_shader_module(device, &shaderModuleDesc);
-
-  shaderModuleDesc.code = fragmentShader;
-  WGpuShaderModule fs = wgpu_device_create_shader_module(device, &shaderModuleDesc);
-
-  WGpuRenderPipelineDescriptor renderPipelineDesc = WGPU_RENDER_PIPELINE_DESCRIPTOR_DEFAULT_INITIALIZER;
-  renderPipelineDesc.vertex.module = vs;
-  renderPipelineDesc.vertex.entryPoint = "main";
-  renderPipelineDesc.fragment.module = fs;
-  renderPipelineDesc.fragment.entryPoint = "main";
-
-  WGpuColorTargetState colorTarget = WGPU_COLOR_TARGET_STATE_DEFAULT_INITIALIZER;
-  colorTarget.format = config.format;
-  renderPipelineDesc.fragment.numTargets = 1;
-  renderPipelineDesc.fragment.targets = &colorTarget;
-
-  renderPipeline = wgpu_device_create_render_pipeline(device, &renderPipelineDesc);
-
-  emscripten_request_animation_frame_loop(raf, 0);
+   WGpuShaderModuleDescriptor shaderModuleDesc = {};
+   shaderModuleDesc.code = vertexShader;
+   WGpuShaderModule vs = wgpu_device_create_shader_module(device, &shaderModuleDesc);
+   shaderModuleDesc.code = fragmentShader;
+   WGpuShaderModule fs = wgpu_device_create_shader_module(device, &shaderModuleDesc);
+   WGpuRenderPipelineDescriptor renderPipelineDesc = WGPU_RENDER_PIPELINE_DESCRIPTOR_DEFAULT_INITIALIZER;
+   renderPipelineDesc.vertex.module = vs;
+   renderPipelineDesc.vertex.entryPoint = "main";
+   renderPipelineDesc.fragment.module = fs;
+   renderPipelineDesc.fragment.entryPoint = "main";
+   WGpuColorTargetState colorTarget = WGPU_COLOR_TARGET_STATE_DEFAULT_INITIALIZER;
+   colorTarget.format = config.format;
+   renderPipelineDesc.fragment.numTargets = 1;
+   renderPipelineDesc.fragment.targets = &colorTarget;
+   renderPipeline = wgpu_device_create_render_pipeline(device, &renderPipelineDesc);
+   emscripten_request_animation_frame_loop(raf, 0);
 }
 
-void ObtainedWebGpuAdapter(WGpuAdapter result, void *userData)
-{
-  adapter = result;
-
-  WGpuDeviceDescriptor deviceDesc = {};
-  wgpu_adapter_request_device_async(adapter, &deviceDesc, ObtainedWebGpuDevice, 0);
+void ObtainedWebGpuAdapter(WGpuAdapter result,void *userData){
+adapter=result;
+WGpuDeviceDescriptor deviceDesc={};
+wgpu_adapter_request_device_async(adapter,&deviceDesc,ObtainedWebGpuDevice,0);
 }
 
-int main()
-{
-  WGpuRequestAdapterOptions options = {};
-  options.powerPreference = WGPU_POWER_PREFERENCE_LOW_POWER;
-  navigator_gpu_request_adapter_async(&options, ObtainedWebGpuAdapter, 0);
+int main(void){
+WGpuRequestAdapterOptions options={};
+options.powerPreference=WGPU_POWER_PREFERENCE_LOW_POWER;
+navigator_gpu_request_adapter_async(&options,ObtainedWebGpuAdapter,0);
 }
