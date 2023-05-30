2f286bd37655178601cfadbba3641129143a0a09
Noah Cohn <noahc42@gmail.com>
2023-05-29 21:41:35 -0700

    Update hello_triangle_minimal.c

--- samples/hello_triangle/hello_triangle_minimal.c
+++ samples/hello_triangle/hello_triangle_minimal.c
@@ -36,17 +36,17 @@ queue=wgpu_device_get_queue(device);
 //   wgpu_canvas_context_configure(canvasContext, &config);
 
 const char *computeShader =
-"@group(0) @binding(0) var<storage,read> inputBuffer: array<f32,64>;
-@group(0) @binding(1) var<storage,read_write> outputBuffer: array<f32,64>;
+"@group(0) @binding(0) var<storage,read> inputBuffer: array<f32,64>;"
+"@group(0) @binding(1) var<storage,read_write> outputBuffer: array<f32,64>;"
 // The function to evaluate for each element of the processed buffer
-fn f(x: f32) -> f32 {
-return 2.0 * x + 1.0;
-}
-@compute @workgroup_size(32)
-fn computeStuff(@builtin(global_invocation_id) id: vec3<u32>) {
+"fn f(x: f32) -> f32 {"
+"return 2.0 * x + 1.0;"
+"}"
+"@compute @workgroup_size(32)"
+"fn computeStuff(@builtin(global_invocation_id) id: vec3<u32>) {"
     // Apply the function f to the buffer element at index id.x:
-outputBuffer[id.x] = f(inputBuffer[id.x]);
-}";
+"outputBuffer[id.x] = f(inputBuffer[id.x]);
+"}";
 
 WGpuShaderModuleDescriptor shaderModuleDesc={};
 shaderModuleDesc.code=computeShader;
