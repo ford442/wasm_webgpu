8e4123c228415e1a602285fc0fd10d4093db817f
Noah Cohn <noahc42@gmail.com>
2023-06-07 05:47:50 -0700

    Update hello_triangle_minimal.cpp

--- samples/hello_triangle/hello_triangle_minimal.cpp
+++ samples/hello_triangle/hello_triangle_minimal.cpp
@@ -118,12 +118,14 @@ std::cout << "at wgpu WGpuOnSubmittedWorkDoneCallback!" << std::endl;
 std::cout << mapBuffer << std::endl;
 };
 
+	/*
 WGpuBufferMapCallback onBuffer=[](WGpuBuffer buffer,void *userData,1,0,bufferSize){
 std::cout << "output: " << std::endl;
 std::cout <<buffer << std::endl;
 };
 const float* output=wgpu_buffer_map_async(mapBuffer,onBuffer,0,1,0,bufferSize);
-
+*/
+	
 wgpu_queue_set_on_submitted_work_done_callback(queue,onComputeDone,0);
 std::cout << "at wgpu_queue_submit_one" << std::endl;
 wgpu_queue_submit_one(queue,commandBuffer);
