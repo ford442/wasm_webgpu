eaa1c950c72590599413d6b1115694354af2b1d2
Noah Cohn <noahc42@gmail.com>
2023-06-07 14:45:20 -0700

    Update hello_triangle_minimal.cpp

--- samples/hello_triangle/hello_triangle_minimal.cpp
+++ samples/hello_triangle/hello_triangle_minimal.cpp
@@ -145,7 +145,7 @@ auto getOutput = wgpu_buffer_get_mapped_range(buffer,uint32_t(0),size);
 std::cout << getOutput << std::endl;
 void * readOutput;
 wgpu_buffer_read_mapped_range(mapBuffer,getOutput,0,&readOutput,bufferSize);
-std::cout << readOutput[0] << std::endl;
+std::cout << readOutput << std::endl;
 };
 std::cout << "at wgpu WGpuOnSubmittedWorkDoneCallback!" << std::endl;
 std::cout << "mapBuffer:" << mapBuffer << std::endl;
