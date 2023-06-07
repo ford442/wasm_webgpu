4c0d405b2cc56f1198c358972e5c03e1bc241879
Noah Cohn <noahc42@gmail.com>
2023-06-07 07:12:54 -0700

    Update hello_triangle_minimal.cpp

--- samples/hello_triangle/hello_triangle_minimal.cpp
+++ samples/hello_triangle/hello_triangle_minimal.cpp
@@ -125,7 +125,8 @@ std::cout << "at mapCallback!" << std::endl;
 
 };
 WGPU_MAP_MODE_FLAGS mode1=0x1;
-wgpu_buffer_map_async(mapBuffer,mapCallback,void *userData,mode1,0,bufferSize);
+void *userData;
+wgpu_buffer_map_async(mapBuffer,mapCallback,&userData,mode1,0,bufferSize);
 wgpu_encoder_end(computePass);
 };
 
