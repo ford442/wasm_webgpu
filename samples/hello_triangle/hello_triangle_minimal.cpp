43d6429c615b0fa2feb5261e91f17919a237e2d9
Noah Cohn <noahc42@gmail.com>
2023-06-07 01:15:58 -0700

    Update hello_triangle_minimal.cpp

--- samples/hello_triangle/hello_triangle_minimal.cpp
+++ samples/hello_triangle/hello_triangle_minimal.cpp
@@ -96,8 +96,8 @@ wgpu_compute_pass_encoder_dispatch_workgroups(computePass,workgroupCount,onE,onE
 // pass.dispatchWorkgroups(workgroupCount, 1, 1);
 // pass.end();
 // encoder.copyBufferToBuffer(outputBuffer,0,mapBuffer,0,bufferSize);
-// std::cout << "at wgpu_command_encoder_finish" << std::endl;
-WGpuCommandBuffer commandBuffer={};// =wgpu_command_encoder_finish(encoder);
+std::cout << "at wgpu_command_encoder_finish" << std::endl;
+WGpuCommandBuffer commandBuffer=wgpu_encoder_finish(encoder);
 std::cout << "at wgpu_queue_submit_one_and_destroy" << std::endl;
 wgpu_queue_submit_one(queue,commandBuffer);
 // bool done=false;
