236a942ff2808368878c8d9d95ddf716d7bc9c43
Noah Cohn <noahc42@gmail.com>
2023-06-07 11:06:55 -0700

    Update hello_triangle_minimal.cpp

--- samples/hello_triangle/hello_triangle_minimal.cpp
+++ samples/hello_triangle/hello_triangle_minimal.cpp
@@ -40,9 +40,9 @@ const char *computeShader =
 "return 2.0 * x + 0.42;"
 "}"
 "@compute @workgroup_size(32)"
-"fn computeStuff(@builtin(global_invocation_id) id: vec3<u32>) {"
+"fn computeStuff(@builtin(global_invocation_id) global_id: vec3<u32>,@builtin(local_invocation_id) local_id: vec3<u32>) {"
     // Apply the function f to the buffer element at index id.x:
-"outputBuffer[id.x] = f(inputBuffer[id.x]);"
+"outputBuffer[global_id.x] = f(inputBuffer[global_id.x]);"
 "}";
 
 void raf(WGpuDevice device){
