f9cce63a287c459e006769eef0c71fb8fbcb4810
Noah Cohn <noahc42@gmail.com>
2023-06-07 01:20:08 -0700

    Update hello_triangle_minimal.cpp

--- samples/hello_triangle/hello_triangle_minimal.cpp
+++ samples/hello_triangle/hello_triangle_minimal.cpp
@@ -97,9 +97,11 @@ wgpu_compute_pass_encoder_dispatch_workgroups(computePass,workgroupCount,onE,onE
 // pass.end();
 // encoder.copyBufferToBuffer(outputBuffer,0,mapBuffer,0,bufferSize);
 std::cout << "at wgpu_command_encoder_finish" << std::endl;
-WGpuCommandBuffer commandBuffer=wgpu_encoder_finish(computePass);
-std::cout << "at wgpu_queue_submit_one_and_destroy" << std::endl;
-wgpu_queue_submit_one(queue,commandBuffer);
+WGpuCommandBufferDescriptor commandBufferDescriptor={};
+WGpuCommandBuffer commandBuffer=wgpu_encoder_finish(encoder);
+// std::cout << "at wgpu_queue_submit_one_and_destroy" << std::endl;
+// wgpu_queue_submit_one(queue,commandBuffer);
+	
 // bool done=false;
 // auto handle=mapBuffer.mapAsync(WGPU_MAP_MODE_READ,0,bufferSize{
 // const float* output=(const float*)mapBuffer.getConstMappedRange(0,bufferSize);
