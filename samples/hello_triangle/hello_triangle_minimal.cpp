0a000b17cc97372eae1cceb8b5b7e7b49b2d0fcf
Noah Cohn <noahc42@gmail.com>
2023-06-07 04:18:34 -0700

    Update hello_triangle_minimal.cpp

--- samples/hello_triangle/hello_triangle_minimal.cpp
+++ samples/hello_triangle/hello_triangle_minimal.cpp
@@ -9,22 +9,6 @@ WGpuDevice device=0;
 // WGpuRenderPipeline renderPipeline;
 WGpuBindGroupLayout bindGroupLayout=0;
 WGpuComputePipeline computePipeline=0;
-
-int bufferSize = 64 * sizeof(float);
-
-const char *computeShader =
-"@group(0) @binding(0) var<storage,read> inputBuffer: array<f32,64>;"
-"@group(0) @binding(1) var<storage,read_write> outputBuffer: array<f32,64>;"
-// The function to evaluate for each element of the processed buffer
-"fn f(x: f32) -> f32 {"
-"return 2.0 * x + 1.0;"
-"}"
-"@compute @workgroup_size(32)"
-"fn computeStuff(@builtin(global_invocation_id) id: vec3<u32>) {"
-    // Apply the function f to the buffer element at index id.x:
-"outputBuffer[id.x] = f(inputBuffer[id.x]);"
-"}";
-
 WGpuQueue queue=0;
 WGpuComputePassDescriptor computePassDescriptor={};
 WGpuBufferDescriptor bufferDescriptor={};
@@ -45,18 +29,28 @@ WGpuBindGroupEntry bindGroupEntry[2]={};
 WGpuBindGroup bindGroup=0;
 WGpuPipelineLayout pipelineLayout=0;
 
+int bufferSize = 64 * sizeof(float);
+
+const char *computeShader =
+"@group(0) @binding(0) var<storage,read> inputBuffer: array<f32,64>;"
+"@group(0) @binding(1) var<storage,read_write> outputBuffer: array<f32,64>;"
+// The function to evaluate for each element of the processed buffer
+"fn f(x: f32) -> f32 {"
+"return 2.0 * x + 1.0;"
+"}"
+"@compute @workgroup_size(32)"
+"fn computeStuff(@builtin(global_invocation_id) id: vec3<u32>) {"
+    // Apply the function f to the buffer element at index id.x:
+"outputBuffer[id.x] = f(inputBuffer[id.x]);"
+"}";
+
+
 void raf(WGpuDevice device){
 queue=wgpu_device_get_queue(device);
 std::cout << "beginning compute commands" << std::endl;
 std::vector<float>input(bufferSize/sizeof(float));
- //   WGpuRenderPassColorAttachment colorAttachment = WGPU_RENDER_PASS_COLOR_ATTACHMENT_DEFAULT_INITIALIZER;
- //   colorAttachment.view = wgpu_texture_create_view(wgpu_canvas_context_get_current_texture(canvasContext), 0);
-//   WGpuRenderPassDescriptor passDesc = {};
 // computePassDescriptor.timestampWriteCount = 0;
 computePassDescriptor.timestampWrites = NULL;
-//   passDesc.numColorAttachments = 1;
-//   passDesc.colorAttachments = &colorAttachment;
- //  WGpuRenderPassEncoder pass = wgpu_command_encoder_begin_render_pass(encoder, &passDesc);
 bufferDescriptor.mappedAtCreation=false;
 bufferDescriptor.size=bufferSize;
 bufferDescriptor.usage=WGPU_BUFFER_USAGE_STORAGE|WGPU_BUFFER_USAGE_COPY_DST;
@@ -74,7 +68,6 @@ input[i]=0.1f*i;
 std::cout << "not skipping input buffer" << std::endl;
 wgpu_queue_write_buffer(queue,inputBuffer,0,input.data(),input.size()*sizeof(float));
 shaderModuleDescriptor={computeShader,0,NULL};
-	// shaderModuleDescriptor.code=computeShader;
 std::cout << "wgpu_device_create_shader_module" << std::endl;
 cs=wgpu_device_create_shader_module(device,&shaderModuleDescriptor);
 std::cout << "create bindgroup layout" << std::endl;
@@ -117,74 +110,54 @@ std::cout << "inputBuffer:\n" << std::endl;
 std::cout << inputBuffer << std::endl;
 std::cout << "dispatch workgroups:" << workgroupCount << ",1,1" << std::endl;
 wgpu_compute_pass_encoder_dispatch_workgroups(computePass,uint32_t(32),uint32_t(1),uint32_t(1));
-// pass.dispatchWorkgroups(workgroupCount, 1, 1);
-// pass.end();
-// encoder.copyBufferToBuffer(outputBuffer,0,mapBuffer,0,bufferSize);
+	// copy output buff
+std::cout << "at wgpu_command_encoder_copy_buffer_to_buffer" << std::endl;
+wgpu_command_encoder_copy_buffer_to_buffer(encoder,outputBuffer,0,mapBuffer,0,bufferSize);
 std::cout << "at wgpu_command_encoder_finish" << std::endl;
 commandBuffer=wgpu_encoder_finish(encoder);
+WGpuOnSubmittedWorkDoneCallback onComputeDone=[](WGpuQueue queue, void *userData){
+std::cout << "at wgpu WGpuOnSubmittedWorkDoneCallback!" << std::endl;
+}
+wgpu_queue_set_on_submitted_work_done_callback(queue,onComputeDone,0);
 std::cout << "at wgpu_queue_submit_one" << std::endl;
 wgpu_queue_submit_one(queue,commandBuffer);
 std::cout << "after wgpu_queue_submit_one" << std::endl;
-// bool done=false;
-// auto handle=mapBuffer.mapAsync(WGPU_MAP_MODE_READ,0,bufferSize{
-// const float* output=(const float*)mapBuffer.getConstMappedRange(0,bufferSize);
-// for(int i=0;i<input.size();++i){
-// mapBuffer.unmap();
-// }
-// done=true;
-// });
-return; // Render just one frame, static content
+auto handle=mapBuffer.mapAsync(WGPU_MAP_MODE_READ,0,bufferSize{
+const float* output=(const float*)mapBuffer.getConstMappedRange(0,bufferSize);
+for(int i=0;i<input.size();++i){
+mapBuffer.unmap();
 }
-
+});
+return;
+}
 
 void ObtainedWebGpuDevice(WGpuDevice result,void *userData){
 device=result;
-//   canvasContext = wgpu_canvas_get_webgpu_context("canvas");
- //  WGpuCanvasConfiguration config = WGPU_CANVAS_CONFIGURATION_DEFAULT_INITIALIZER;
-// config.device = device;
-//   config.format = navigator_gpu_get_preferred_canvas_format();
-//   wgpu_canvas_context_configure(canvasContext, &config);
 raf(device);
 }
-
-//   WGpuShaderModule vs = wgpu_device_create_shader_module(device, &shaderModuleDesc);
-//   shaderModuleDesc.code = fragmentShader;
 /*
+//   WGpuShaderModule vs = wgpu_device_create_shader_module(device, &shaderModuleDesc);
 WGpuProgrammableStageDescriptor stageDesc{};
 stageDesc.module=cs;
 stageDesc.entryPoint="computeStuff";
 stageDesc.constantCount=0;
 stageDesc.constants=NULL;
+WGpuRenderPipelineDescriptor renderPipelineDesc = WGPU_RENDER_PIPELINE_DESCRIPTOR_DEFAULT_INITIALIZER;
+//  skipped somehow
+WGpuComputePipelineDescriptor computePipelineDesc={};
+computePipelineDesc.compute=stageDesc;
+wgpu_device_create_compute_pipeline(WGpuDevice device,WGpuShaderModule computeModule, const char *entryPoint NOTNULL,WGpuPipelineLayout layout,const WGpuPipelineConstant *constants,int numConstants);
+ emscripten_set_main_loop(raf,0);
  */
-//   WGpuRenderPipelineDescriptor renderPipelineDesc = WGPU_RENDER_PIPELINE_DESCRIPTOR_DEFAULT_INITIALIZER;
- //  skipped somehow
-// WGpuComputePipelineDescriptor computePipelineDesc={};
-// computePipelineDesc.compute=stageDesc;
- //  renderPipelineDesc.vertex.module = vs;
-//   renderPipelineDesc.vertex.entryPoint = "main";
-//   renderPipelineDesc.fragment.module = fs;
-//   renderPipelineDesc.fragment.entryPoint = "main";
- //  WGpuColorTargetState colorTarget = WGPU_COLOR_TARGET_STATE_DEFAULT_INITIALIZER;
- //  colorTarget.format = config.format;
- //  renderPipelineDesc.fragment.numTargets = 1;
-//   renderPipelineDesc.fragment.targets = &colorTarget;
-// wgpu_device_create_compute_pipeline(WGpuDevice device,WGpuShaderModule computeModule, const char *entryPoint NOTNULL,WGpuPipelineLayout layout,const WGpuPipelineConstant *constants,int numConstants);
-//   emscripten_set_main_loop(raf,0);
 
 void ObtainedWebGpuAdapter(WGpuAdapter result,void *userData){
 adapter=result;
 WGpuDeviceDescriptor deviceDescriptor={};
 wgpu_adapter_request_device_async(adapter,&deviceDescriptor,ObtainedWebGpuDevice,0);
-// wgpu_adapter_request_device_async_simple(adapter,ObtainedWebGpuDevice);
-// wgpu_adapter_request_device_sync(adapter,&deviceDescriptor);
-// wgpu_adapter_request_device_sync_simple(adapter);
-	// ObtainedWebGpuDevice();
 }
 
 int main(void){
 WGpuRequestAdapterOptions options={};
 options.powerPreference=WGPU_POWER_PREFERENCE_HIGH_PERFORMANCE;
-// navigator_gpu_request_adapter_sync_simple();
 navigator_gpu_request_adapter_async(&options,ObtainedWebGpuAdapter,0);
-// navigator_gpu_request_adapter_async_simple(ObtainedWebGpuAdapter);
 }
